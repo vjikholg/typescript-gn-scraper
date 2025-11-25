1f62c150258cfb50e4b6e2d54b7cafa98a198734
vjikholg <yuvict1@outlook.com>
2025-11-24 18:18:07 -0800

    added MathNode[] type to represent complex HTML-math expressions recursively.

--- src/types.ts
+++ src/types.ts
@@ -46,3 +46,15 @@ interface Series {
     type: string; 
     series: number[]; // keep as ids -> sql: getById, etc., 
 }
+
+/**
+ * Abstract syntax tree representation of Math nodes which Group Names uses to represent fractions, square roots
+ * Doing it this way persists data more easily and translates well into LaTeX
+ */
+export type MathNode = 
+    | {type: 'text'; value: string}
+    | {type: 'fraction'; numerator: MathNode[]; denominator: MathNode[]}
+    | {type: 'sqrt'; value: MathNode[]}
+    | {type: 'sub'; value: MathNode[]}
+    | {type: 'sup'; value: MathNode[]}
+    | {type: 'group'; tag: MathNode[], children: MathNode[]}
