4c98ca27190bd35548151df4b447ecb59eb730e8
zagszy <yuvict1@outlook.com>
2025-12-10 17:43:27 -0800

    Added additional type structure to GraphTheory.ts
    SVGScraper.vertexGrabber finished. Need to sub replacement for SVGPathElement.getPathData() to complete edge collection algorithm.

--- src/scraper/SVGScraper.ts
+++ src/scraper/SVGScraper.ts
@@ -1,10 +1,17 @@
-import {Page, ElementHandle} from 'puppeteer'
-import { Vertex } from '../types/GraphTheory';
+import { Page, ElementHandle } from 'puppeteer'
+import { Vertex, Edge } from '../types/GraphTheory';
 
 
 /**
  * Grab node information off groupnames, and export into format readable by force-graph or D3.js
+ * TODO - Globally we need to define a Map<string, GroupInfo>, which returns references by raw-label 
+ * I.e., raw innerHTML from HTMLelem in question 
+ * Actually since we're scraping group-names as MathNodes, it'll be more useful returning GroupInfo
+ *  
  */
+
+const Regex : RegExp = /(M)(\d+?.\d+?|\d+) (\d+?.\d+) (L)(\d+?.\d+?) (\d+.\d+)/ // incomplete but we'll use as placeholder for now
+
 export class SVGScraper { 
     static async search(page : Page) : Promise<ElementHandle<Element>> { 
         const svd : ElementHandle<Element> | null = await page.$('.svd')
@@ -22,22 +29,45 @@ export class SVGScraper {
             throw new Error(`Something went wrong scraping SVGPathElement`)
         }
 
-        const path : SVGPathElement = await rawpath.evaluate((elem) => {
+        const path : SVGPathElement = await rawpath.evaluate((elem : SVGPathElement) => {
             return elem;
         })
         return path;
     } 
 
     static processPathElement(paths: SVGPathElement) { 
+        const rawHTML : string = paths.innerHTML; // SVGPathElement.getPathData() isn't working, use regex + innerHTML
+        // kind of impossible without getPathData() :\
+        
+        
 
     }
 
     static async vertexGrabber(svd : ElementHandle<Element>) : Promise<Vertex[]> { 
         const vtc : Vertex[] = [];
-        const subgroups : any = svd.$$('.nsg .csg')
+        const subgroups : ElementHandle<HTMLElement> = svd.$$('.nSg .cSg')
         
-        return vtc;
+        for (const subgroup of subgroups) { 
+            // scrapes top to bottom - sub1 -> sub2 -> ... 
+            // decide what we want label to be; do we want vertex to hold info?    
+            // const subgroup = subgroups[i]; 
 
+            const style : CSSStyleDeclaration = await subgroup.$eval((elem : HTMLElement) => { 
+                // grab styling 
+                return elem.style as CSSStyleDeclaration;
+            }); 
+
+            const left : string = style.left; 
+            const top : string = style.top; 
+            // assign anchor as label for now
+            
+            const label : string = subgroup.$eval('a', (el : HTMLAnchorElement) => { 
+                return el.innerText.trim();
+            })
+            // we'll need to get id = sub{i} -> link information from subdesc and proper highlighting within D3-force 
+            vtc.push({label: label, x: left, y: top} as Vertex); 
+        }
+        return vtc;
     }
 
 
--- src/types/GraphTheory.ts
+++ src/types/GraphTheory.ts
@@ -1,5 +1,22 @@
-export type Vertex = {}; 
-export type Edge = Vertex[][] 
+/**
+ * Rudimentary Graph-Theory library sufficient to represent data to be exported to D3-force or similar.     
+ */
+
+export type Vertex = {
+    label? : string | number; // we'll assign this by raw label - later on we'll organize groups by id (in order of which they're scraped) - should save memory 
+    x: string | number; 
+    y: string | number; 
+};
+
+export type Vertices = Vertex[]
+
+export type Edge = { // ts passes obj by reference in creation - pseudo pter
+    v1: Vertex; 
+    v2: Vertex; 
+};
+
+
+export type Edges = Edge[]; 
 
 export type Graph = {
     vtc: Vertex[]
